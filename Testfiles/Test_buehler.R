3f80d0f2563142bbb9f4ff41b319bdb912e22409
HIT-91-MAN <buehler1991@gmail.com>
2021-02-27 15:09:21 +0100

    adding testfiles to directory starting to write on historical expalantion of btc acessing btc data on yahoofinance

---
+++ Testfiles/Test_buehler.R
@@ -0,0 +1,33 @@
+# testfile Buehler
+# this file is only for testing purpose, does not appear in final paper
+
+# acessing data via coinmarket cap https://coinmarketcap.com/
+# from yahoofinance ticker BTC-USD
+# *Close price adjusted for splits.
+#**Adjusted close price adjusted for both dividends and splits.
+
+
+getSymbols("BTC-USD")
+
+BTC_USD=na.rm(`BTC-USD`)
+
+
+#
+head(BTC_USD,n=5)
+tail(BTC_USD,n=3)      #  
+
+x_level = Cl(BTC_USD)   # 
+log_ret   = diff(log(Cl(BTC_USD))) # 
+
+# Line Chart
+chartSeries(BTC_USD,type="line", theme=chartTheme("white"))
+
+# Bar Chart
+chartSeries(BTC_USD, type="bar",theme=chartTheme("white"))
+
+# Candle Stick Chart
+chartSeries(BTC_USD, type="candlesticks", theme=chartTheme("white"))
+
+# 
+
+
