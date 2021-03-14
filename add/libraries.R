2f546778344d2f448f53c14f40f7614e80ec1fd7
majimaken <kengeeler@gmail.com>
2021-03-14 19:11:23 +0100

    Started working on 3.1.2. BP algorithm

--- add/libraries.R
+++ add/libraries.R
@@ -6,21 +6,10 @@ library(quantmod)
 library(forecast)
 library(fGarch)
 library(devtools)
-<<<<<<< HEAD
-=======
-
->>>>>>> main
 #library(MDFA)
 #library(neuralnet)
-
-#library(MDFA) Knitting vom Rmd funktioniert sust nid
 library(neuralnet)
-<<<<<<< HEAD
-=======
-
->>>>>>> main
 library(boot)
 #library(plyr) 
 library(MASS)
-library(MDFS)
 
