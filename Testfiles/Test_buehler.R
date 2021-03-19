f80be2b57fe4ec0fb2fad4d0d9bc185f9a2f2037
HIT-91-MAN <buehler1991@gmail.com>
2021-03-19 19:40:51 +0100

    working on historical background created 2 plots log prices  and price ,  added history of bc

--- Testfiles/Test_buehler.R
+++ Testfiles/Test_buehler.R
@@ -173,9 +173,49 @@ estimate_nn<-function(train_set,number_neurons,data_mat,test_set,f) # function
 
 
 
+# plotig data bitcoin prices ####  
+# for theroretical background
+
+
+# acessing data via coinmarket cap https://coinmarketcap.com/
+# from yahoofinance ticker BTC-USD
+# *Close price adjusted for splits.s
+#**Adjusted close price adjusted for both dividends and splits.
+
+load("data/BTC_USD_13_03_21.rda") 
+
+BTC_USD =BTC_USD_13_03_21
+
+cl_btc=Cl(BTC_USD)
+cl_btc_log=log(cl_btc)
+
+
+first=min(cl_btc["2019-06-26::2020-09-01"]) # maximales value zwischen datum 
+cl_btc[which(cl_btc==first) ]               # 
+
+events <- xts(LETTERS[1:5], as.Date(c("2015-01-14","2017-12-16","2018-12-15","2019-06-26","2020-03-12")))
+
+
+plot.xts(Cl(BTC_USD),col = "black", main="Bitcoin in US $",lwd = 0.8)
+addEventLines(events,srt=90,pos=2,lty=3,col = c("darkgreen","red","darkgreen","red","darkgreen"),lwd=2,)
+
+
+plot.xts(cl_btc_log,col = "black", main="Logarithmic Bitcoin in US $",lwd = 0.8)
+addEventLines(events,srt=90,pos=2,lty=3,col = c("darkgreen","red","darkgreen","red","darkgreen"),lwd=2,)
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+2014-09-20
+
+plot.xts(test)
