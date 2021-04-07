c30a88088383e9d79ef65673e2a9f0957f15b193
HIT-91-MAN <buehler1991@gmail.com>
2021-04-08 00:02:26 +0200

    input generic first looping

---
+++ Code/combination_input_grid.R
@@ -0,0 +1,35 @@
+
+
+
+# this function returns a matrix with all combination of neurons : note vectors with 1 0 0 0 .. are still in the output
+# but 100 ...1 not 
+
+
+combination_input_grid=function(maxlayer,maxneuron)
+  
+{ 
+ starttime=Sys.time()
+  
+ input = vector(mode = "list", length = maxlayer)
+ input[[1]]=1:maxneuron
+ if( maxlayer > 1){for(i in 2:maxlayer){input[[i]]=0:maxneuron}}
+
+ grid=expand.grid( input ) 
+ 
+
+ truevec=rep(TRUE,dim(grid)[1])
+ for(row in 1:dim(grid)[1])
+ {
+   for(col in (2:(dim(grid)[2]-1)) )
+   {
+     if  ( grid[row,col]< grid[row,col+1] & grid[row,col]==0){truevec[row]=FALSE } 
+   }
+ }
+ combmat=grid[truevec,]
+ 
+ print(paste("duration: " ,Sys.time()-starttime))
+   
+   
+ return(combmat)
+} 
+
