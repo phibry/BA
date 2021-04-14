c1d2613281550705b3b3b69abf945fd81dcac0fe
HIT-91-MAN <buehler1991@gmail.com>
2021-04-14 21:31:16 +0200

    file fitting distribution begin with test train split writing

---
+++ images/generating distributions on logrets.R
@@ -0,0 +1,30 @@
+# generating density plots form dt an normaldist
+library(MASS)
+
+load("data/log_ret_27_03_21.rda")  # loading logreturns closing! data xts
+logret=log_ret_27_03_21 
+
+my_data=as.numeric(logret[,1])   # as vector 
+fit <- fitdistr(my_data, densfun="normal")  
+fit2<- fitdistr(my_data, "t", start = list(m=mean(my_data),s=sd(my_data), df=1), lower=c(-1, 0.001,1)) #algo braucht startwerte
+
+dt_ls <-function(x,mu,sigma,df){1/sigma * dt((x - mu)/sigma, df)}# converting scale and location in only df
+
+beschrnorm= paste("Normal distribution ~ N(","mu=",as.character(round(fit$estimate[1],5)),
+                  "sd=",as.character(round(fit$estimate[2],5)),")")
+beschrt= paste("T - distribution ~ t(","l=",as.character(round(fit2$estimate[1],5)),"s=",
+               as.character(round(fit2$estimate[2],5)),"df=",as.character(round(fit2$estimate[3],5)),")")
+
+
+
+
+
+# plotting the data
+hist(my_data, pch=20, breaks=70, prob=TRUE, main="Logreturns BTC",xlab="logrets")
+curve(dnorm(x, fit$estimate[1], fit$estimate[2]), col="red", lwd=1.5, add=T)
+curve(dt_ls(x,fit2$estimate[1],fit2$estimate[2],fit2$estimate[3]), col="blue", lwd=1.5, add=T)
+legend("topleft", inset=.02, legend=c(beschrnorm,beschrt),
+       col=c("red", "blue"), pch = c(1,1), cex=1,2, horiz=F)
+
+
+       
