7d144e29180634a9d24069d7128799c857c07fe4
Philipp Rieser <philipp.rieser@gmail.com>
2021-04-21 08:15:39 +0200

    batch5 data batch5 plots all with lag=7

---
+++ data/batch_5/README.md
@@ -0,0 +1,61 @@
+# Datasets Batch 5
+
+Find optimal net architecture
+
+## General Information
+- Datasets with lag=7
+- Start: 2020-01-01
+- End: 2021-03-27
+- The in- and out-of-sample MSE and Sharpe were calculated
+- 9 Datasets with a rolling in/out split
+- 6m in-sample
+- 1m out-of-sample
+- All with max 10 neurons and 3 layers with 50 realizations
+- 1110 n-l-combinations
+- 50*1110 = 55500 nets trained for each in/out split
+
+## About the datasets
+### nn_10_3_50_1.rda"
+- In-Sample: 2020-01-01 - 2020-06-30 (175)
+- Out-of-Sample: 2020-07-01 - 2020-07-31 (31)
+- Time taken to calculate: 34min
+
+### nn_10_3_50_2.rda"
+- In-Sample: 2020-02-01 - 2020-07-31 (175)
+- Out-of-Sample: 2020-08-01 - 2020-08-31 (31)
+- Time taken to calculate: 28min
+
+### nn_10_3_50_3.rda"
+- In-Sample: 2020-03-01 - 2020-08-31 (177) 
+- Out-of-Sample: 2020-09-01 - 2020-09-30 (30) 
+- Time taken to calculate: 31min
+
+### nn_10_3_50_4.rda"
+- In-Sample: 2020-04-01 - 2020-09-30 (176) 
+- Out-of-Sample: 2020-10-01 - 2020-10-31 (28) 
+- Time taken to calculate: 2h 21min
+
+### nn_10_3_50_5.rda"
+- In-Sample: 2020-05-01 - 2020-10-31 (175) 
+- Out-of-Sample: 2020-11-01 - 2020-11-30 (30) 
+- Time taken to calculate: 1h 39min
+
+### nn_10_3_50_6.rda"
+- In-Sample: 2020-06-01 - 2020-11-30 (174)
+- Out-of-Sample: 2020-12-01 - 2020-12-31 (31)
+- Time taken to calculate: 2h 9min
+
+### nn_10_3_50_7.rda"
+- In-Sample: 2020-07-01 - 2020-12-31 (175)
+- Out-of-Sample: 2021-01-01 - 2021-01-31 (31)
+- Time taken to calculate: 1h 2min
+
+### nn_10_3_50_8.rda"
+- In-Sample: 2020-08-01 - 2021-01-31 (175)
+- Out-of-Sample: 2021-02-01 - 2021-02-28 (28)
+- Time taken to calculate: 1h 33min
+
+### nn_10_3_50_9.rda"
+- In-Sample: 2020-09-01 - 2021-02-29 (172)
+- Out-of-Sample: 2021-03-01 - 2021-03-27 (27)
+- Time taken to calculate: 3h 33min
---
+++ data/batch_5/plots_batch_5.R
@@ -0,0 +1,209 @@
+source("add/libraries.r") 
+source("add/Functions.r")
+# Load Files####
+load("data/batch_5/nn_10_3_50_1.rda")
+load("data/batch_5/nn_10_3_50_2.rda")
+load("data/batch_5/nn_10_3_50_3.rda")
+load("data/batch_5/nn_10_3_50_4.rda")
+load("data/batch_5/nn_10_3_50_5.rda")
+load("data/batch_5/nn_10_3_50_6.rda")
+load("data/batch_5/nn_10_3_50_7.rda")
+load("data/batch_5/nn_10_3_50_8.rda")
+load("data/batch_5/nn_10_3_50_9.rda")
+
+
+# ACF####
+# Plot Price
+load("data/log_ret_27_03_21.rda")
+subseti <- log_ret_27_03_21["2020-01-01::"]
+
+chart.ACF(log_ret_27_03_21, maxlag = 15, main="Max Length")
+chart.ACF(log_ret_27_03_21["2020-01-01::2021-03-27"], maxlag = 15, main="Subset")
+
+chart.ACF(log_ret_27_03_21["2020-01-01::2020-07-31"], maxlag = 15, main="Subset 1")
+chart.ACF(log_ret_27_03_21["2020-02-01::2020-08-31"], maxlag = 15, main="Subset 2")
+chart.ACF(log_ret_27_03_21["2020-03-01::2020-09-30"], maxlag = 15, main="Subset 3")
+chart.ACF(log_ret_27_03_21["2020-04-01::2020-10-31"], maxlag = 15, main="Subset 4")
+chart.ACF(log_ret_27_03_21["2020-05-01::2020-11-30"], maxlag = 15, main="Subset 5")
+chart.ACF(log_ret_27_03_21["2020-06-01::2020-12-31"], maxlag = 15, main="Subset 6")
+chart.ACF(log_ret_27_03_21["2020-07-01::2021-01-31"], maxlag = 15, main="Subset 7")
+chart.ACF(log_ret_27_03_21["2020-08-01::2021-02-28"], maxlag = 15, main="Subset 8")
+chart.ACF(log_ret_27_03_21["2020-09-01::2021-03-27"], maxlag = 15, main="Subset 9")
+
+
+# Merge Files####
+mse_in_mean_1 <- apply(X=nn_10_3_50_1[, seq(1, 200, 4)], MARGIN=1, FUN=mean)
+mse_in_mean_2 <- apply(X=nn_10_3_50_2[, seq(1, 200, 4)], MARGIN=1, FUN=mean)
+mse_in_mean_3 <- apply(X=nn_10_3_50_3[, seq(1, 200, 4)], MARGIN=1, FUN=mean)
+mse_in_mean_4 <- apply(X=nn_10_3_50_4[, seq(1, 200, 4)], MARGIN=1, FUN=mean)
+mse_in_mean_5 <- apply(X=nn_10_3_50_5[, seq(1, 200, 4)], MARGIN=1, FUN=mean)
+mse_in_mean_6 <- apply(X=nn_10_3_50_6[, seq(1, 200, 4)], MARGIN=1, FUN=mean)
+mse_in_mean_7 <- apply(X=nn_10_3_50_7[, seq(1, 200, 4)], MARGIN=1, FUN=mean)
+mse_in_mean_8 <- apply(X=nn_10_3_50_8[, seq(1, 200, 4)], MARGIN=1, FUN=mean)
+mse_in_mean_9 <- apply(X=nn_10_3_50_9[, seq(1, 200, 4)], MARGIN=1, FUN=mean)
+
+
+mse_in <- as.data.frame(cbind(
+  apply(X=nn_10_3_50_1[, seq(1, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_2[, seq(1, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_3[, seq(1, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_4[, seq(1, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_5[, seq(1, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_6[, seq(1, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_7[, seq(1, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_8[, seq(1, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_9[, seq(1, 200, 4)], MARGIN=1, FUN=mean)
+));colnames(mse_in) <- 1:9
+
+mse_out <- as.data.frame(cbind(
+  apply(X=nn_10_3_50_1[, seq(2, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_2[, seq(2, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_3[, seq(2, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_4[, seq(2, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_5[, seq(2, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_6[, seq(2, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_7[, seq(2, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_8[, seq(2, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_9[, seq(2, 200, 4)], MARGIN=1, FUN=mean)
+));colnames(mse_out) <- 1:9
+
+sharpe_in <- as.data.frame(cbind(
+  apply(X=nn_10_3_50_1[, seq(3, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_2[, seq(3, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_3[, seq(3, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_4[, seq(3, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_5[, seq(3, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_6[, seq(3, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_7[, seq(3, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_8[, seq(3, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_9[, seq(3, 200, 4)], MARGIN=1, FUN=mean)
+));colnames(sharpe_in) <- 1:9
+
+sharpe_out <- as.data.frame(cbind(
+  apply(X=nn_10_3_50_1[, seq(4, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_2[, seq(4, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_3[, seq(4, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_4[, seq(4, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_5[, seq(4, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_6[, seq(4, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_7[, seq(4, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_8[, seq(4, 200, 4)], MARGIN=1, FUN=mean),
+  apply(X=nn_10_3_50_9[, seq(4, 200, 4)], MARGIN=1, FUN=mean)
+));colnames(sharpe_out) <- 1:9
+
+save(mse_in, mse_out, sharpe_in, sharpe_out, file = "data/batch_5/meanerino.rda")
+
+load("data/batch_5/meanerino.rda")
+
+
+#.####
+# 1#### 
+# width=1400
+# height=900
+mse_1 <- nn_10_3_50_1[,c(rbind(seq(1, 200, 4), seq(2, 200, 4)))]
+plot_all_rect(mati=mse_1, real=50, title="MSE Split: 1")
+legend("center", legend=c("1 Layer", '2 Layers', '3 Layers'), pch=15, pt.cex=2, cex=0.8, bty='n',
+       col = c('#FF00004D', '#0000FF4D', '#80FF004D'), horiz=TRUE, title="Layer partitioning")
+plot_by_layer_rect(mati=mse_1, real=50, title="MSE Split: 1")
+
+# 2####
+mse_2 <- nn_10_3_50_2[,c(rbind(seq(1, 200, 4), seq(2, 200, 4)))]
+# plot_all_rect(mati=mse_2, real=50, title="In/Out Split: 2")
+plot_all_rect_scale(mati=mse_2, real=50, title="MSE Split: 2")
+legend("center", legend=c("1 Layer", '2 Layers', '3 Layers'), pch=15, pt.cex=2, cex=0.8, bty='n',
+       col = c('#FF00004D', '#0000FF4D', '#80FF004D'), horiz=TRUE, title="Layer partitioning")
+# plot_by_layer_rect(mati=mse_2, real=50, title="In/Out Split: 2")
+plot_by_layer_rect_scale(mati=mse_2, real=50, title="MSE Split: 2",
+                         scale_vec=c(2, 2, 4))
+
+# 3####
+mse_3 <- nn_10_3_50_3[,c(rbind(seq(1, 200, 4), seq(2, 200, 4)))]
+# plot_all_rect(mati=mse_3, real=50, title="In/Out Split: 3")
+plot_all_rect_scale(mati=mse_3, real=50, title="MSE Split: 3")
+legend("center", legend=c("1 Layer", '2 Layers', '3 Layers'), pch=15, pt.cex=2, cex=0.8, bty='n',
+       col = c('#FF00004D', '#0000FF4D', '#80FF004D'), horiz=TRUE, title="Layer partitioning")
+# plot_by_layer_rect(mati=mse_3, real=50, title="In/Out Split: 3")
+plot_by_layer_rect_scale(mati=mse_3, real=50, title="MSE Split: 3",
+                         scale_vec=c(3, 3, 3))
+# 4####
+mse_4 <- nn_10_3_50_4[,c(rbind(seq(1, 200, 4), seq(2, 200, 4)))]
+# plot_all_rect(mati=mse_4, real=50, title="In/Out Split: 4")
+plot_all_rect_scale(mati=mse_4, real=50, title="MSE Split: 4", scale_fac=9)
+legend("center", legend=c("1 Layer", '2 Layers', '3 Layers'), pch=15, pt.cex=2, cex=0.8, bty='n',
+       col = c('#FF00004D', '#0000FF4D', '#80FF004D'), horiz=TRUE, title="Layer partitioning")
+# plot_by_layer_rect(mati=mse_4, real=50, title="In/Out Split: 4")
+plot_by_layer_rect_scale(mati=mse_4, real=50, title="MSE Split: 4",
+                         scale_vec=c(4, 8, 10))
+
+# 5####
+mse_5 <- nn_10_3_50_5[,c(rbind(seq(1, 200, 4), seq(2, 200, 4)))]
+# plot_all_rect(mati=mse_5, real=50, title="In/Out Split: 5")
+plot_all_rect_scale(mati=mse_5, real=50, title="MSE Split: 5", scale_fac=11)
+legend("center", legend=c("1 Layer", '2 Layers', '3 Layers'), pch=15, pt.cex=2, cex=0.8, bty='n',
+       col = c('#FF00004D', '#0000FF4D', '#80FF004D'), horiz=TRUE, title="Layer partitioning")
+
+# plot_by_layer_rect(mati=mse_5, real=50, title="In/Out Split: 5")
+plot_by_layer_rect_scale(mati=mse_5, real=50, title="MSE Split: 5",
+                         scale_vec=c(10, 10, 10))
+
+# 6####
+mse_6 <- nn_10_3_50_6[,c(rbind(seq(1, 200, 4), seq(2, 200, 4)))]
+# plot_all_rect(mati=mse_6, real=50, title="In/Out Split: 6")
+plot_all_rect_scale(mati=mse_6, real=50, title="MSE Split: 6", scale_fac=7)
+legend("center", legend=c("1 Layer", '2 Layers', '3 Layers'), pch=15, pt.cex=2, cex=0.8, bty='n',
+       col = c('#FF00004D', '#0000FF4D', '#80FF004D'), horiz=TRUE, title="Layer partitioning")
+# plot_by_layer_rect(mati=mse_6, real=50, title="In/Out Split: 6")
+plot_by_layer_rect_scale(mati=mse_6, real=50, title="MSE Split: 6",
+                         scale_vec=c(10, 25, 10))
+
+# 7####
+mse_7 <- nn_10_3_50_7[,c(rbind(seq(1, 200, 4), seq(2, 200, 4)))]
+# plot_all_rect(mati=mse_7, real=50, title="In/Out Split: 7")
+plot_all_rect_scale(mati=mse_7, real=50, title="MSE Split: 7", scale_fac=10)
+legend("center", legend=c("1 Layer", '2 Layers', '3 Layers'), pch=15, pt.cex=2, cex=0.8, bty='n',
+       col = c('#FF00004D', '#0000FF4D', '#80FF004D'), horiz=TRUE, title="Layer partitioning")
+# plot_by_layer_rect(mati=mse_7, real=50, title="In/Out Split: 7")
+plot_by_layer_rect_scale(mati=mse_7, real=50, title="MSE Split: 7",
+                         scale_vec=c(35, 25, 5))
+
+# 8####
+mse_8 <- nn_10_3_50_8[,c(rbind(seq(1, 200, 4), seq(2, 200, 4)))]
+# plot_all_rect(mati=mse_8, real=50, title="In/Out Split: 8")
+plot_all_rect_scale(mati=mse_8, real=50, title="MSE Split: 8", scale_fac=5)
+legend("center", legend=c("1 Layer", '2 Layers', '3 Layers'), pch=15, pt.cex=2, cex=0.8, bty='n',
+       col = c('#FF00004D', '#0000FF4D', '#80FF004D'), horiz=TRUE, title="Layer partitioning")
+# plot_by_layer_rect(mati=mse_8, real=50, title="In/Out Split: 8")
+plot_by_layer_rect_scale(mati=mse_8, real=50, title="MSE Split: 8",
+                         scale_vec=c(30, 15, 5))
+
+# 9####
+mse_9 <- nn_10_3_50_9[,c(rbind(seq(1, 200, 4), seq(2, 200, 4)))]
+# plot_all_rect(mati=mse_9, real=50, title="In/Out Split: 9")
+plot_all_rect_scale(mati=mse_9, real=50, title="MSE Split: 9", scale_fac=7)
+legend("center", legend=c("1 Layer", '2 Layers', '3 Layers'), pch=15, pt.cex=2, cex=0.8, bty='n',
+       col = c('#FF00004D', '#0000FF4D', '#80FF004D'), horiz=TRUE, title="Layer partitioning")
+# plot_by_layer_rect(mati=mse_9, real=50, title="In/Out Split: 9")
+plot_by_layer_rect_scale(mati=mse_9, real=50, title="MSE Split: 9",
+                         scale_vec=c(10, 20, 8))
+
+
+
+
+
+# Mean Plots####
+split_colors <- c("#59C7EB", "#E0607E", "#0A9086", "#FEA090", "#3E5496", "#EFDC60", "#8E2043", "#9AA0A7", "#AC9A8C")
+
+load("data/batch_5/meanerino.rda")
+plot_mse_mean(mse_in=mse_in, mse_out=mse_out, title="Mean MSE over all 9 splits", 20)
+legend("left", legend=c("1 Layer", '2 Layers', '3 Layers'), pch=15, pt.cex=2, cex=0.8, bty='n',
+       col = c('#FF00004D', '#0000FF4D', '#80FF004D'), horiz=TRUE, title="Layer partitioning")
+legend("right", legend=c("1", "2", "3", "4", "5", "6", "7", "8", "9"), pch=15, pt.cex=2, cex=0.8, bty='n',
+       col = split_colors, horiz=TRUE, title="Train/Test Splits")
+
+
+plot_sharpe_mean(sharpe_in=sharpe_in, sharpe_out=sharpe_out, title="Mean Sharpe over all 9 splits")
+legend("left", legend=c("1 Layer", '2 Layers', '3 Layers'), pch=15, pt.cex=2, cex=0.8, bty='n',
+       col = c('#FF00004D', '#0000FF4D', '#80FF004D'), horiz=TRUE, title="Layer partitioning")
+legend("right", legend=c("1", "2", "3", "4", "5", "6", "7", "8", "9"), pch=15, pt.cex=2, cex=0.8, bty='n',
+       col = split_colors, horiz=TRUE, title="Train/Test Splits")
+
