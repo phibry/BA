e2297f3b163bf2d0281dc8443fdf4369b05b4bdc
majimaken <kengeeler@gmail.com>
2021-04-24 22:21:09 +0200

    Sharpe ratio and structuring

--- images/generating_flowchart_overview.R
+++ images/generating_flowchart_overview.R
@@ -8,8 +8,8 @@ grViz(diagram = "digraph flowchart {
   node [fontname = arial, shape = oval, color = Black, style = 'solid']
   tab1 [label = '@@1']
   tab2 [label = '@@2']
-  tab3 [label = '@@3']
-  tab4 [label = '@@4', shape = diamond]
+  tab3 [label = '@@3', shape = diamond]
+  tab4 [label = '@@4']
   tab5 [label = '@@5']
 
   tab1 -> tab2 -> tab3 -> tab4 -> tab5;
@@ -17,7 +17,7 @@ grViz(diagram = "digraph flowchart {
 }
   [1]: '1) Data exploration'
   [2]: '2) Selection of a suitable neural network'
-  [3]: '3) Explainable artificial intelligence (XAI)'    
-  [4]: '4) Predictive accuracy' 
+  [3]: '3) Predictive accuracy'    
+  [4]: '4) Explainable artificial intelligence (XAI)' 
   [5]: '5) Implementation trading strategy'
   ")
