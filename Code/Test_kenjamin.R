8c9e38bd329717309b648f552b124968130be3c6
majimaken <kengeeler@gmail.com>
2021-04-25 10:27:50 +0200

    GARCH vola prediction

--- Code/Test_kenjamin.R
+++ Code/Test_kenjamin.R
@@ -127,3 +127,75 @@ bottom_gjr <- quantile(vola_mat$GJR_Volatility, 0.05)
 signals_sell <- (vola_mat$GJR_Volatility > upper_gjr) * -1
 signals_buy <- (vola_mat$GJR_Volatility < bottom_gjr) * 1
 signals_gjr <- signals_sell + signals_buy
+
+
+
+
+
+
+
+
+
+
+
+# Load
+source("add/libraries.R")
+source("add/Functions.R")
+load("data/BTC_USD_27_03_21.rda")
+load("data/log_ret_27_03_21.rda")
+
+##### Volatility prediction for trading signals: Method 1 using fGArch #####
+# Using functions from fGarch package
+
+# Data prep prices
+BTC <- BTC_USD_27_03_21$`BTC-USD.Close`
+BTC <- BTC[-1,]
+BTC <- na.omit(BTC)
+
+# Data prep log returns
+dat <- log_ret_27_03_21
+colnames(dat) <- "BTC log returns"
+head(dat)
+
+# Transform to data frame for ugarch functions
+dat <- as.data.frame(dat)
+dat <- na.omit(dat)
+
+# Define date from which we start to predict volatilities
+# Define index for dat (class = data frame)
+
+split <- as.Date("2020-05-01")
+
+ind_1 <- index(dat)[which(rownames(dat) == split)]
+dat[ind_1,]
+
+# GARCH fit
+
+GARCH_spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1,1)), mean.model = list(armaOrder = c(0,0)))
+
+# GARCH predictions
+# Rolling window 
+
+mod = ugarchroll(GARCH_spec, data = dat, n.ahead = 1, 
+                 n.start = ind_1,  refit.every = 1, refit.window = "recursive", 
+                  fit.control = list(),
+                 calculate.VaR = TRUE, VaR.alpha = 0.05,
+                 keep.coef = TRUE)
+
+GARCH_vola <- mod@forecast$density
+
+#save(GARCH_vola, file = "data/GARCH_vola_predictions/GARCH_vola_predictions.rda")
+
+
+
+
+
+
+
+
+##### Inspection of GARCH vola #####
+
+# Distribution of GARCH
+
+
+
