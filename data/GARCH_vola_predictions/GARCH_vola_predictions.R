9d04313f0b7278d2aa78e69df29515361f397253
majimaken <kengeeler@gmail.com>
2021-05-09 13:04:07 +0200

    Student t for GARCH

--- data/GARCH_vola_predictions/GARCH_vola_predictions.R
+++ data/GARCH_vola_predictions/GARCH_vola_predictions.R
@@ -29,25 +29,50 @@ dat <- na.omit(dat)
 # Define index for dat (class = data frame)
 # 1.7.2020 - 27.03.2021
 
-split <- as.Date("2019-07-01")
+split <- as.Date("2017-07-01")
 
 ind_1 <- index(dat)[which(rownames(dat) == split)]
 dat[ind_1,]
 
-##### GARCH fit #####
+# QQ-Plot normal
+GARCH_spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1,1)), 
+                         mean.model = list(armaOrder = c(1,1), include.mean = TRUE), distribution.model = "norm")
+GARCH_fit <- ugarchfit(GARCH_spec, data = dat)
+# Define residuals for normal
+eps <- GARCH_fit@fit$residuals # residuals
+z_t <- eps / GARCH_fit@fit$sigma # standardized residuals
+theor <- pdist(distribution = "norm", sort(z_t))
+emp <- rank(sort(z_t))/length(z_t)
+par(mfrow = c(2,1))
+plot(x = emp, y = theor, main = "QQ-Plot normal distribution")
 
-GARCH_spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1,1)), mean.model = list(armaOrder = c(1,1)))
+# QQ-Plot student t
+GARCH_spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1,1)), 
+                         mean.model = list(armaOrder = c(1,1), include.mean = TRUE), distribution.model = "std")
+GARCH_fit <- ugarchfit(GARCH_spec, data = dat)
+# Define residuals for student t
+eps <- GARCH_fit@fit$residuals # residuals
+z_t <- eps / GARCH_fit@fit$sigma # standardized residuals
+theor <- pdist(distribution = "std", sort(z_t), shape = GARCH_fit@fit$coef['shape'])
+emp <- rank(sort(z_t))/length(z_t)
+plot(x = emp, y = theor, main = "QQ-Plot student t") # student t looks slightly better
 
-# GARCH predictions
-# Rolling window 
+# Therefore, we use GARCH with student t
+GARCH_spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1,1)), 
+                         mean.model = list(armaOrder = c(1,1), include.mean = TRUE), distribution.model = "std")
+
+# GARCH one-step-ahead forecasts with rolling window
+# Window size = 365
+# Refit GARCH every months (30 time steps)
 
 mod <- ugarchroll(GARCH_spec, data = dat, n.ahead = 1, 
-                 n.start = ind_1,  refit.every = 7, refit.window = "recursive", 
-                 window.size = 2*365, 
+                 n.start = ind_1,  refit.every = 30, refit.window = "moving", 
+                 window.size = 365, 
                  fit.control = list(),
                  calculate.VaR = FALSE,
                  keep.coef = TRUE)
 
+par(mfrow = c(1,1))
 plot(mod)
 2
 0
@@ -56,10 +81,10 @@ plot(mod)
 
 GARCH_vola <- mod@forecast$density
 
-# Adding trading signals 
+# Trading strategy: Trading based on historical volatility as threshold 
 # Generate trading signal if predicted vola >= 0.95 level of historical vola
 
-th <- sd(dat$`BTC log returns`) * 2 # 1.64 = two-sided 0.95
+th <- sd(dat$`BTC log returns`[1:(ind_0)]) * 1.64 # 1.64 = two-sided 0.95
 
 for(i in 1:nrow(GARCH_vola)){
   if(GARCH_vola$Sigma[i] >= th){
@@ -70,7 +95,7 @@ for(i in 1:nrow(GARCH_vola)){
   }
 }
 
-# Adding signum trading signals
+# Trading strategy: Trading based on sign of mean forecast (based on ARMA(1,1))
 
 GARCH_vola$Signum_Signal <- sign(GARCH_vola$Mu)
 
@@ -99,6 +124,7 @@ sharpe_sign <- sqrt(365)*mean(perf_sign,na.rm=T)/sqrt(var(perf_sign,na.rm=T))
 bh <- rep(1, length(perf)) * dat_xts[(ind_0+1):ind_1]
 sharpe_bh <- sqrt(365)*mean(bh, na.rm = T) / sqrt(var(bh, na.rm = T))
 
+# Plot performances
 par(mfrow = c(3,1))
 plot(cumsum(perf), main = paste("Trading performance historical with sharpe: ", sharpe))
 plot(cumsum(perf_sign), main = paste("Trading performance signum with sharpe: ", sharpe_sign))
