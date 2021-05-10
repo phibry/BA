1433293261ebe87c2d6c2be2cf32e8c26582b91c
HIT-91-MAN <buehler1991@gmail.com>
2021-05-10 16:20:21 +0200

    cahangeiong function xai outp for insample derivative

--- add/Functions.R
+++ add/Functions.R
@@ -1627,8 +1627,9 @@ OLPDphil <- function(in_out_sample_separator,data_mat,use_in_samp=T,neuron_vec) 
   return(list(OLPD_mat=OLPD_mat, lm_obj=lm_obj))
 }
 
-xai_outp<-function(x,lags,in_out_sep,neuron_vec,use_in_samp=F,anz=1000,percentage=NULL,devi=1,plot=F,outtarget,use_between=F,lower=2,upper=3)
+xai_outp<-function(x,lags,in_out_sep,neuron_vec,intercept=F,anz=1000,percentage=NULL,devi=1,plot=F,outtarget,use_between=F,lower=2,upper=3)
 {
+  
   data_function(x, lags, in_out_sep, autoassign = T)
   
   target_in<-data_mat[paste("/",in_out_sep,sep=""),1]
@@ -1651,19 +1652,13 @@ xai_outp<-function(x,lags,in_out_sep,neuron_vec,use_in_samp=F,anz=1000,percentag
   delta<-1.e-5
   epsilon<-1.e-4
   
+  data_in<-train_set
+  data_xts_in<-train_set_xts
   
   
-  if (use_in_samp)
-  {
-    # Smoother (in-sample data)
-    data<-train_set
-    data_xts<-train_set_xts
-  } else
-  {
-    # Rougher (out-sample data)
-    data<-test_set
-    data_xts<-test_set_xts
-  } 
+  data_out<-test_set
+  data_xts_out<-test_set_xts
+  
   
   for (l in 1:anz)  
   {  
@@ -1671,12 +1666,12 @@ xai_outp<-function(x,lags,in_out_sep,neuron_vec,use_in_samp=F,anz=1000,percentag
     nn <- neuralnet(f,data=train_set,hidden=neuron_vec,linear.output=T)
     net=estimate_nn(train_set,number_neurons=neuron_vec,data_mat,test_set,f,newnet = F,nn=nn)
     
-    
-    for (i in 1:(nrow(data)))
+    ###olpd insample 
+    for (i in 1:(nrow(data_in)))
     {
-      x<-matrix(data[i,2:ncol(data)],nrow=1)
-      colnames(x)<-colnames(data)[2:ncol(data)]
-      OLPD_scaled_obj<-OLPD_func(x,delta,epsilon,nn)
+      x_in<-matrix(data_in[i,2:ncol(data_in)],nrow=1)
+      colnames(x_in)<-colnames(data_in)[2:ncol(data_in)]
+      OLPD_scaled_obj<-OLPD_func(x_in,delta,epsilon,nn)
       
       if (i==1)
       {
@@ -1686,18 +1681,45 @@ xai_outp<-function(x,lags,in_out_sep,neuron_vec,use_in_samp=F,anz=1000,percentag
         OLPD_scaled_mat<-rbind(OLPD_scaled_mat,OLPD_scaled_obj$effect)
       }
     }
-    OLPD_mat<-transform_OLPD_back_original_data_func(data_xts,data_mat,OLPD_scaled_mat,lm_obj,data)$OLPD_mat
+    OLPD_mat_in<-transform_OLPD_back_original_data_func(data_xts_in,data_mat,OLPD_scaled_mat,lm_obj,data_in)$OLPD_mat
+    
+    #olpd out of sample
+    for (i in 1:(nrow(data_out)))
+    {
+      x_out<-matrix(data_out[i,2:ncol(data_out)],nrow=1)
+      colnames(x_out)<-colnames(data_out)[2:ncol(data_out)]
+      OLPD_scaled_obj<-OLPD_func(x_out,delta,epsilon,nn)
+      
+      if (i==1)
+      {
+        OLPD_scaled_mat<-OLPD_scaled_obj$effect
+      } else
+      {
+        OLPD_scaled_mat<-rbind(OLPD_scaled_mat,OLPD_scaled_obj$effect)
+      }
+    }
+    OLPD_mat_out<-transform_OLPD_back_original_data_func(data_xts_out,data_mat,OLPD_scaled_mat,lm_obj,data_out)$OLPD_mat    
     
     
+    
+    
+    #### calclating mean 
     if (l==1)
     {
-      start=OLPD_mat
+      #olpd in and out
+      start_in=OLPD_mat_in
+      start_out=OLPD_mat_out
+      #nn
       startsignal_in=sign(net$predicted_nn_in_sample)
       startsignal_out=sign(net$predicted_nn)  
     }
     else
     {
-      start=start+OLPD_mat 
+      #olpd in and out
+      
+      start_in=start_in+OLPD_mat_in
+      start_out=start_out+OLPD_mat_out
+      #nn
       startsignal_in=startsignal_in+sign(net$predicted_nn_in_sample)
       startsignal_out=startsignal_out+sign(net$predicted_nn) 
     }
@@ -1707,17 +1729,60 @@ xai_outp<-function(x,lags,in_out_sep,neuron_vec,use_in_samp=F,anz=1000,percentag
   
   
   
+  #renaming
   
-  index(OLPD_mat)<-index(data_xts)  
+  index(OLPD_mat_in)<-index(data_xts_in)  
+  index(OLPD_mat_out)<-index(data_xts_out) 
+  
   #mean of all olpd over realisations
-  OLPD_mat=na.exclude(start/anz)
+  
+  OLPD_mat_in=na.exclude(start_in/anz)
+  OLPD_mat_out=na.exclude(start_out/anz)
+  
+  
+  mean_in=apply(OLPD_mat_in[-nrow(OLPD_mat_in),],2,mean)
+  sd_in=apply(OLPD_mat_in[-nrow(OLPD_mat_in),],2,sd)
+  
+  apply(OLPD_mat_out,2,sd)
+  apply(OLPD_mat_out,2,mean)
+  
+  
+  if(intercept==F){OLPD_mat_out=OLPD_mat_out[,-1];mean_in=mean_in[-1];sd_in=sd_in[-1]}
+  
+  OLPD_mat_out_deviator=OLPD_mat_out
+  OLPD_mat_out_deviator[]<-0
+  
+  for (k in 1:dim(OLPD_mat_out_deviator)[2])
+    
+    
+    OLPD_mat_out_deviator[which(OLPD_mat_out[,k]   > mean_in[k] + devi*sd_in[k] | OLPD_mat_out[,k]   < mean_in[k] - devi*sd_in[k]),k]<-1
+  
+  
+  
+  
+  
+  
+  sum_explana=as.xts(apply(OLPD_mat_out_deviator,1,sum))
+  signal_olpd<-sum_explana
+  signal_olpd[]=1
+  signal_olpd[which(sum_explana>=lower & sum_explana < upper )]<- 0.5
+  signal_olpd[which(sum_explana >= upper )]<-0
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   #signals ##########################################################--------------------------------------------
   
   # neuralnet signals
   
   #signum of the most votet 50% negative + 50 % positive get a zero , >50% negative & <50%positive get a -1  
- 
+  
   signal_in=sign(startsignal_in)
   signal_out=sign(startsignal_out)
   majority=signal_out # return all signals from nets
@@ -1736,38 +1801,13 @@ xai_outp<-function(x,lags,in_out_sep,neuron_vec,use_in_samp=F,anz=1000,percentag
   
   
   
-  deviate= function(x,deviationscaling=devi) # devi is assigned before 
-    #this function checks if value is > mean + scaling * std and returns 1 if true 0 if not
-  {
-    if(sum(is.na(x))!=0){print("nas detected")}
-    x=abs(x)
-    deviatevalind=which(x>mean(x)+deviationscaling*stdev(x))
-    x[]=FALSE
-    x[deviatevalind]=TRUE
-    return(x)
-  }
-  
-  
-  deviatmat=apply(OLPD_mat[,-1],2,deviate)
-  sum_explana=as.xts(apply(deviatmat,1,sum))
-  signal_olpd<-sum_explana
-  signal_olpd[]=1
-  signal_olpd[which(sum_explana>=lower & sum_explana < upper )]<- 0.5
-  signal_olpd[which(sum_explana >= upper )]<-0
-  
   
   sum(signal_out==sign(target_out))/length(signal_out) # out of sample accuracy
   sum(signal_in==sign(target_in[paste("::",as.Date(in_out_sep)-1,sep="")]))/length(signal_in) # in sample accuracy
   
   
   
-  signal_nn_and_olpd=signal_out
-  
-  signal_nn_and_olpd[which(signal_olpd==0)]<-0
-  if(use_between){ signal_nn_and_olpd[which(signal_olpd==0.5)]<-0.5}
-  
-  
-  
+  signal_nn_and_olpd=signal_out;signal_nn_and_olpd[which(signal_olpd==0)]<-0;#signal_nn_and_olpd[which(signal_olpd==0.5)]<-0.5
   perf_nn_out_with_olpd=signal_nn_and_olpd*target_out
   
   
@@ -1776,17 +1816,31 @@ xai_outp<-function(x,lags,in_out_sep,neuron_vec,use_in_samp=F,anz=1000,percentag
   sharpe_net_olpd=round(sqrt(365)*SharpeRatio(perf_nn_out_with_olpd,FUN="StdDev"),3)
   
   #plots####--------------------------------------------------------------------------------------------------------------------------
-  
+  plot.new()  
   if(plot)
   {
-    plot.new()
-    par(mfrow=c(4,2))
+    
+    #par(mfrow=c(4,2))
     #plot of decision
     
     
-    plot(OLPD_mat,col=rainbow(ncol(OLPD_mat)))
-    for (i in 1:ncol(OLPD_mat))
-      mtext(colnames(OLPD_mat)[i],col=rainbow(ncol(OLPD_mat))[i],line=-i)
+    plot(OLPD_mat_out,col=rainbow(ncol(OLPD_mat_out)))
+    for (i in 1:ncol(OLPD_mat_out))
+      mtext(colnames(OLPD_mat_out)[i],col=rainbow(ncol(OLPD_mat_out))[i],line=-i)
+    
+    abline(h=as.numeric(mean_in[-1]))
+    abline(h=as.numeric(mean_in[-1]+devi*sd_in[-1]),col="red")
+    abline(h=as.numeric(mean_in[-1]-devi*sd_in[-1]),col="green")
+    
+    
+    
+    
+    
+    plot(OLPD_mat_in,col=rainbow(ncol(OLPD_mat_in)))
+    for (i in 1:ncol(OLPD_mat_in))
+      mtext(colnames(OLPD_mat_in)[i],col=rainbow(ncol(OLPD_mat_in))[i],line=-i)
+    
+    
     
     
     plot(signal_olpd,main="final signal")
@@ -1809,13 +1863,13 @@ xai_outp<-function(x,lags,in_out_sep,neuron_vec,use_in_samp=F,anz=1000,percentag
     plot(cumsum(perf_nn_out_with_olpd),main=paste("Net Olpd, sharpe:",as.character(sharpe_net_olpd)))
   }
   
+  
+  
   return(list(sharpe_net_olpd=sharpe_net_olpd,sharpe_net=sharpe_net,sharpe_bh=sharpe_bh
               ,perf_nn_out_with_olpd=perf_nn_out_with_olpd,perf_nn_out=perf_nn_out,signal_out=signal_out,signal_olpd=signal_olpd,majority=majority)) 
   
 }
-
-
-
+s
 
 #.####
 
