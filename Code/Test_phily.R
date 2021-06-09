971f36f575232bf418bb3f51fbc641d8ea2656ca
Philipp Rieser <philipp.rieser@gmail.com>
2021-06-10 00:13:37 +0200

    Figure 6, 7 letters

--- Code/Test_phily.R
+++ Code/Test_phily.R
@@ -1064,3 +1064,27 @@ events<- xts(LETTERS[1:6], as.Date(c("2020-08-01","2020-09-04","2020-10-18","202
 addEventLines(events, srt=0, pos=1, lty=3, col = rep("#004c6d",6),lwd=2, cex=1.2)
 # lines(signal, on=NA, lwd=2, col="red", ylim=c(-1.3, 1.3))
 
+
+# Plot Letters####
+load("data/BTC_USD_27_03_21.rda") 
+
+BTC_USD <- BTC_USD_27_03_21
+
+cl_btc <- Cl(BTC_USD)
+cl_btc_log <- log(cl_btc)
+
+
+first <- min(cl_btc["2019-06-26::2020-09-01"]) # maximales / minamales value zwischen datum 
+
+# events generieren für values
+events <- xts(LETTERS[1:5], as.Date(c("2015-01-14","2017-12-16","2018-12-15","2019-06-26","2020-03-12")))
+
+#plots
+plot.xts(Cl(BTC_USD), col = "black", main="Bitcoin in US $",lwd = 0.8)
+addEventLines(events,srt=0,pos=1,lty=3,col = c("darkgreen","red","darkgreen","red","darkgreen"),lwd=2, cex=1.2)
+?addEventLines
+
+
+
+plot.xts(cl_btc_log,col = "black", main="Logarithmic Bitcoin in US $",lwd = 0.8)
+addEventLines(events,srt=0,pos=1,lty=3,col = c("darkgreen","red","darkgreen","red","darkgreen"),lwd=2, cex=1.2)
